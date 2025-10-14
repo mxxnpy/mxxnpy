53ce2bd26632366bee886ae742e2cab40562496b
mxxnpy <ayu.le@icloud.com>
2025-10-14 19:07:15 -0300

    .

--- today.py
+++ today.py
@@ -5,7 +5,6 @@ import os
 from lxml import etree
 import time
 import hashlib
-import pandas 
 
 # Fine-grained personal access token with All Repositories access:
 # Account permissions: read:Followers, read:Starring, read:Watching
